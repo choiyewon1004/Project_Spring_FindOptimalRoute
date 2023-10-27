c60c1ae171b3e671f0b2caa28a796e509ff6ccc1
choiyewon <choiyewon1997@gmail.com>
2023-10-27 18:30:31 +0900

    Initial commit

---
+++ src/main/java/com/example/project_spring_optimalroute/ProjectSpringOptimalrouteApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_optimalroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringOptimalrouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringOptimalrouteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_spring_optimalroute/ProjectSpringOptimalrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_optimalroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringOptimalrouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
